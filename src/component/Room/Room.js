b698e8c23d7951ea3f82592bc9eb2d8e5ee06f07
yanisahmed <yanisahmed12@gmail.com>
2021-10-31 18:58:31 +0600

    HomePage Updated

--- src/component/Room/Room.js
+++ src/component/Room/Room.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Room = ({ room }) => {
+    console.log(room);
     return (
-        <div className="single-room w-72 mx-auto">
+        <div className="single-room w-72 mx-auto border-b-2">
             <img src={room.image_url} alt="" className="mb-2 w-72 h-64" />
-            <div className="flex justify-between">
+            <p className="text-1xl clear-both mb-4">{room.description.substr(0, 60)}</p>
+            <div className="flex justify-between mb-4">
                 <div className="">
-                    <h3>{room.title}</h3>
+                    <h3 className="text-yellow-700">{room.title}</h3>
                     <h4>$<span>{room.price}</span>/night</h4>
                 </div>
                 <Link to={`/rooms/${room._id}`}><button className="mr-2 border border-yellow-500 py-2 px-4 ">Book</button></Link>
